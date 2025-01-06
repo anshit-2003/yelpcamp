095f9427664269e01719c59d80683dafd797c0a8
Anshit Bakshi <84705984+anshit-2003@users.noreply.github.com>
2025-01-06 11:37:58 +0530

    Changing Files

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
-if(process.env.NODE_ENV !== 'production'){
-    require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
 }
 
-
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const ejsMate = require('ejs-mate');
-const session = require('express-session');
-const flash = require('connect-flash');
-const ExpressError = require('./utils/ExpressError');
-const methodOverride = require('method-override');
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const User = require('./models/user');
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
+const session = require("express-session");
+const flash = require("connect-flash");
+const ExpressError = require("./utils/ExpressError");
+const methodOverride = require("method-override");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 const port = process.env.port || 3000;
-const mongoSanitize = require('express-mongo-sanitize');
-const helmet = require('helmet');
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
 
-
-
-const userRoutes = require('./routes/users');
-const campgroundRoutes = require('./routes/campgrounds');
-const reviewRoutes = require('./routes/reviews');
-const MongoDBStore = require('connect-mongo')(session)
+const userRoutes = require("./routes/users");
+const campgroundRoutes = require("./routes/campgrounds");
+const reviewRoutes = require("./routes/reviews");
+const MongoDBStore = require("connect-mongo")(session);
 const dbUrl = process.env.DB_URL;
 mongoose.connect(dbUrl);
 
@@ -35,30 +32,32 @@ db.once("open", () => {
 
 const app = express();
 
-app.engine('ejs', ejsMate)
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(mongoSanitize({
-    replaceWith: '_',
-  }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+    mongoSanitize({
+        replaceWith: "_",
+    })
+);
 
-const secret = process.env.SECRET || 'thisshouldbeabettersecret!'
+const secret = process.env.SECRET || "thisshouldbeabettersecret!";
 const store = new MongoDBStore({
-    url : dbUrl,
+    url: dbUrl,
     secret,
-    touchAfter : 24 * 60 * 60 
-})
+    touchAfter: 24 * 60 * 60,
+});
 
-store.on('error',function (e) {
-    console.log('session store error!!!!',e)
-})
+store.on("error", function (e) {
+    console.log("session store error!!!!", e);
+});
 const sessionConfig = {
     store,
-    name: 'session',
+    name: "session",
     secret,
     resave: false,
     saveUninitialized: true,
@@ -66,12 +65,12 @@ const sessionConfig = {
         httpOnly: true,
         // secure:true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
-}
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+};
 
-app.use(session(sessionConfig))
-app.use(flash()); 
+app.use(session(sessionConfig));
+app.use(flash());
 app.use(helmet());
 const scriptSrcUrls = [
     "https://stackpath.bootstrapcdn.com/",
@@ -109,14 +108,13 @@ app.use(
                 "'self'",
                 "blob:",
                 "data:",
-                "https://res.cloudinary.com/dz80pvk4x/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://res.cloudinary.com/dz80pvk4x/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
                 "https://images.unsplash.com/",
             ],
             fontSrc: ["'self'", ...fontSrcUrls],
         },
     })
 );
-
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -127,35 +125,29 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
     res.locals.currentUser = req.user;
-    res.locals.success = req.flash('success');
-    res.locals.error = req.flash('error');
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
     next();
-})
-
-
-app.use('/', userRoutes);
-app.use('/campgrounds', campgroundRoutes)
-app.use('/campgrounds/:id/reviews', reviewRoutes)
-
-
-app.get('/', (req, res) => {
-    res.render('home')
 });
 
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
 
-app.all('*', (req, res, next) => {
-    next(new ExpressError('Page Not Found', 404))
-})
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.all("*", (req, res, next) => {
+    next(new ExpressError("Page Not Found", 404));
+});
 
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
-    if (!err.message) err.message = 'Oh No, Something Went Wrong!'  
-    res.status(statusCode).render('error', { err })
-})
+    if (!err.message) err.message = "Oh No, Something Went Wrong!";
+    res.status(statusCode).render("error", { err });
+});
 
 app.listen(port, () => {
-    console.log(`Serving on port ${port}`)
-    
-})
-
-
+    console.log(`Serving on port ${port}`);
+});
